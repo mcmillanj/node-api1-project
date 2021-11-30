4abddb4cc88b6846c7db55dd56bb01f77ffd436f
therman mcmillan <mcmillanj0106@yahoo.com>
2021-11-29 19:42:32 -0500

    4 passed so far

--- api/server.js
+++ api/server.js
@@ -3,6 +3,15 @@ const express = require('express')
 const users = require('./users/model')
 const server = express()
 
+
+
+server.post('/api/users', (req,res) => {
+
+  user.insert(user)  
+})
+
+
+
 server.get('/api/users', (req, res) => {
 //  console.log('gettjing all users')  
 users.find() 
@@ -21,7 +30,12 @@ server.get('/api/users/:id', (req, res) => {
     //  console.log('gettjing all users')  
     users.findById(req.params.id)
     .then(user => {
-        console.log(user)
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist", 
+            })           
+        }
+        
         res.json(user)
     })
     .catch(err =>{
