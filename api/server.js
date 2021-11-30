0a3df0098aca664aea54aba04447df6f963493f7
therman mcmillan <mcmillanj0106@yahoo.com>
2021-11-29 19:19:01 -0500

    4 passed so far

--- api/server.js
+++ api/server.js
@@ -1,3 +1,50 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const users = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+//  console.log('gettjing all users')  
+users.find() 
+.then(users => {
+    res.json(users)
+})
+.catch(err =>{
+res.status(500).json({
+    message: 'error getting users',
+    err:err.message,
+})
+
+}) 
+})
+server.get('/api/users/:id', (req, res) => {
+    //  console.log('gettjing all users')  
+    users.findById(req.params.id)
+    .then(user => {
+        console.log(user)
+        res.json(user)
+    })
+    .catch(err =>{
+    res.status(500).json({
+        message: 'error getting user',
+        err:err.message,
+    })
+    
+    }) 
+    })
+    
+
+
+
+
+
+
+
+
+server.use('*', (req, res) => {
+res.status(404).json({
+    message: ' not found'
+})
+
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(5000, () => {
+    console.log('server listening on 5000')
+})
